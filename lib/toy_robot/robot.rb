5cae33d62d06253c848743b20eff8643891e89c7
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-26 22:03:10 -0700

    Refactor `move` method

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -25,12 +25,7 @@ module ToyRobot
     end
 
     def move
-      case @direction
-      when "NORTH" then move_north
-      when "SOUTH" then move_south
-      when "EAST" then move_east
-      when "WEST" then move_west
-      end
+      send("move_#{@direction.downcase}")
     end
   end
 end
