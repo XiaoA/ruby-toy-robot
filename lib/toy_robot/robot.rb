f96b4e9e8cf0ceb07d96c69af487b8b23545bb5c
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-27 08:46:00 -0700

    Refactor `turn_left` method

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,5 +1,7 @@
 module ToyRobot
   class Robot
+    DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
+
     attr_reader :east, :north, :direction
 
     def initialize(east = 0, north = 0, direction = "NORTH")
@@ -29,12 +31,7 @@ module ToyRobot
     end
 
     def turn_left
-      @direction = case @direction
-                   when "NORTH" then "WEST"
-                   when "SOUTH" then "EAST"
-                   when "EAST" then "NORTH"
-                   when "WEST" then "SOUTH"
-                   end
+      @direction = DIRECTIONS[DIRECTIONS.index(@direction) - 1]
     end
   end
 end
